82fbe911043d68daaf19a298ef415b2c64a76bc0
Kemal <Kemal@Kemal-MacBook-Pro.local>
2016-05-22 21:21:01 +0300

    Image and Stroop Test Websites

---
+++ README.md
@@ -0,0 +1,1 @@
+# StressTest
---
+++ stroopImageSequence.js
@@ -0,0 +1,23 @@
+var stroopImageSequence = [
+'resources/images/stroopTest/blue-green.png',
+'resources/images/stroopTest/blue-yellow.png',
+'resources/images/stroopTest/green-blue.png',
+'resources/images/stroopTest/green-red.png',
+'resources/images/stroopTest/red-red.png',
+'resources/images/stroopTest/red-yellow.png',
+'resources/images/stroopTest/yellow-blue.png',
+'resources/images/stroopTest/yellow-green.png',
+'resources/images/stroopTest/yellow-blue.png',
+'resources/images/stroopTest/blue-green.png',
+'resources/images/stroopTest/green-red.png',
+'resources/images/stroopTest/red-red.png',
+'resources/images/stroopTest/green-blue.png',
+'resources/images/stroopTest/blue-green.png',
+'resources/images/stroopTest/green-red.png',
+'resources/images/stroopTest/green-blue.png',
+'resources/images/stroopTest/red-red.png',
+'resources/images/stroopTest/green-blue.png',
+'resources/images/stroopTest/red-red.png',
+'resources/images/stroopTest/yellow-blue.png'];
+var colorAnswers = ['blue','blue','green','green','red','red','yellow','yellow','yellow','blue','green','red','green','blue','green','green','red','green','red','yellow'];
+var wordAnswers = ['green','yellow','blue','red','red','yellow','blue','green','blue','green','red','red','blue','green','red','blue','red','blue','red','blue'];
