467fbd15dfd3e0394be0478a2dbbca730c193993
kemal <kbeskardesler@gmail.com>
2018-04-26 14:48:01 +0300

    Added HR and Temperature graphs and analyze feature

--- stroopImageSequence.js
+++ stroopImageSequence.js
@@ -21,3 +21,5 @@ var stroopImageSequence = [
 'resources/images/stroopTest/yellow-blue.png'];
 var colorAnswers = ['blue','blue','green','green','red','red','yellow','yellow','yellow','blue','green','red','green','blue','green','green','red','green','red','yellow'];
 var wordAnswers = ['green','yellow','blue','red','red','yellow','blue','green','blue','green','red','red','blue','green','red','blue','red','blue','red','blue'];
+
+var imageCodes = [ 34, 32, 43, 41, 11, 12, 23, 24, 23, 34, 41, 11, 43, 34, 41, 43, 11, 43, 11, 23 ];
